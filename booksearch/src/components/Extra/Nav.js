7430626d6cd19965a82ed310ca6aa8d929144204
sebasp-125 <sp6027193@gmail.com>
2024-03-04 18:53:02 -0500

    Actualizacion con Google. Agregamos Login y Register.
    Se termino los Estilos de Login y Register, Y se inicio el Ladin Peague. -Sebastian Perez-

--- booksearch/src/components/Extra/Nav.js
+++ booksearch/src/components/Extra/Nav.js
@@ -22,7 +22,7 @@ export default function Nav() {
                 </ol>
                 <div className="flex-none gap-2">
                     <button onClick={() => LoginForm()} style={{ background: "white", color: "black", padding: "10px", borderRadius: "12px", width:"100%" }}>Login/Register</button>
-                    <div className="dropdown dropdown-end">
+                    <div className="dropdown dropdown-end"> 
                         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
                                 <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
