48192154ec503e6d6e12d39284340c933dcad1cb
sebasp-125 <sp6027193@gmail.com>
2024-03-04 14:28:22 -0600

    EXTRA

--- booksearch/src/components/Extra/Nav.js
+++ booksearch/src/components/Extra/Nav.js
@@ -3,21 +3,19 @@ import React from 'react'
 export default function Nav() {
     return (
         <div>
-            <div className="navbar bg-base-100" style={{ backgroundColor: "rgb(72, 66, 69, 0.52)" , color: "white" }}>
+            <div className="navbar bg-base-100" style={{ backgroundColor: "rgb(72, 66, 69, 0.52)", color: "white" }}>
                 <div className="flex-1">
                     <a className="btn btn-ghost text-xl">Book | Quest</a>
                 </div>
-                <ol style={{ marginRight: "50px", gap: "60px", cursor: "pointer" , fontweight:"bold" }}>
-                    <li>Nav 1</li>
-                    <li>Nav 2</li>
-                    <li>Nav 3</li>
-                    <li>Nav 4</li>
-                    <li>Nav 5</li>
+                <ol style={{ marginRight: "50px", gap: "60px", cursor: "pointer", fontweight: "bold" }}>
+                    <li style={{borderBottom:"3px solid black" , BorderRadius: "50px"}}>Inicio</li>
+                    <li>Eventos</li>
+                    <li>Estanteria</li>
+                    <li>Nuevos</li>
+                    <li>Colaboladores</li>
                 </ol>
                 <div className="flex-none gap-2">
-                    <div className="form-control">
-                        <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-                    </div>
+                    <button style={{ background: "white", color: "black", padding: "10px", borderRadius: "12px", width:"100%" }}>Login/Register</button>
                     <div className="dropdown dropdown-end">
                         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
