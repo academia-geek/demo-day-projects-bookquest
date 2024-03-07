b5830756ae304b3de89cf842c6e32fb4ef7f7316
JDCO123 <josedavidcastilloospina@gmail.com>
2024-03-07 11:42:07 -0500

    Se agrega el navbar al login y register y se modifican algunos estilos.

--- booksearch/src/components/Login.js
+++ booksearch/src/components/Login.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { Formik, Form, Field } from 'formik';
 import { useDispatch } from 'react-redux';
 import { actionGoogle } from '../redux/actionsLogin';
+import Nav from './Extra/Nav';
 
 export default function Login() {
     const navigate = useNavigate();
@@ -22,42 +23,45 @@ export default function Login() {
         navigate('/');
     }
     return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row">
-                <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">¡Inicia Sesión!</h1>
-                    <p className="py-6">Explora, Descubre, Sumérgete en un Mar de Conocimiento en la Biblioteca Digital.</p>
-                </div>
-                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <Formik
-                        initialValues={{ username: '', password: '' }}
-                        onSubmit={handleSubmit}
-                    >
-                        {({ isSubmitting }) => (
-                            <Form className="card-body">
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Nombre de Usuario</span>
-                                    </label>
-                                    <Field type="text" name="username" placeholder="Nombre de Usuario" className="input input-bordered" required />
-                                </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Contraseña</span>
-                                    </label>
-                                    <Field type="password" name="password" placeholder="Contraseña" className="input input-bordered" required />
-                                    <label className="label">
-                                        <a href="#" className="label-text-alt link link-hover">Olvidaste tu Contraseña?</a>
-                                        <button className="btn btn-warning" onClick={() => RegistroFrom()}>No tienes cuenta aún?</button>
-                                    </label>
-                                </div>
-                                <div className="form-control mt-6">
-                                    <button type="submit" className="btn btn-active" disabled={isSubmitting} onClick={() => HomeForm()}>Login</button><br></br>
-                                    <button type="submit" className="btn btn-active" disabled={isSubmitting}><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1920px-Google_2015_logo.svg.png' alt='' width={"80px"} onClick={() => dispatch(actionGoogle())}></img>¿Quieres Iniciar con Google?.</button>
-                                </div>
-                            </Form>
-                        )}
-                    </Formik>
+        <div>
+            <Nav />
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">¡Inicia Sesión!</h1>
+                        <p className="py-6">Ingresa y comienza a navegar en nuestra nube de libros, siéntete libre de encontrar lo que necesitas, cuando lo necesitas.</p>
+                    </div>
+                    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <Formik
+                            initialValues={{ username: '', password: '' }}
+                            onSubmit={handleSubmit}
+                        >
+                            {({ isSubmitting }) => (
+                                <Form className="card-body">
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Nombre de Usuario</span>
+                                        </label>
+                                        <Field type="text" name="username" placeholder="Nombre de Usuario" className="input input-bordered" required />
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Contraseña</span>
+                                        </label>
+                                        <Field type="password" name="password" placeholder="Contraseña" className="input input-bordered" required />
+                                        <label className="label">
+                                            <a href="#" className="label-text-alt link link-hover">Olvidaste tu Contraseña?</a>
+                                            <button className="btn btn-warning" onClick={() => RegistroFrom()}>No tienes cuenta aún?</button>
+                                        </label>
+                                    </div>
+                                    <div className="form-control mt-6">
+                                        <button type="submit" className="btn btn-active" disabled={isSubmitting} onClick={() => HomeForm()}>Login</button><br></br>
+                                        <button type="submit" className="btn btn-active" disabled={isSubmitting}><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1920px-Google_2015_logo.svg.png' alt='' width={"80px"} onClick={() => dispatch(actionGoogle())}></img>¿Quieres Iniciar con Google?.</button>
+                                    </div>
+                                </Form>
+                            )}
+                        </Formik>
+                    </div>
                 </div>
             </div>
         </div>
--- booksearch/src/components/Register.js
+++ booksearch/src/components/Register.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import '../Styles/StylesRegister.css';
 import { useDispatch } from 'react-redux';
+import Nav from './Extra/Nav';
 
 export default function Register() {
     const dispatch = useDispatch();
@@ -29,53 +30,56 @@ export default function Register() {
     };
 
     return (
-        <div className="container">
-            <div className="image-container">
-                <img src="https://res.cloudinary.com/dwpzqxijx/image/upload/v1709680646/AppPerfil/tb9gpqtaxq2efs3dardn.png" alt="Placeholder" />
-            </div>
-            <div className="form-container card">
-                <h2>Registro de Usuario</h2>
-                <form onSubmit={handleSubmit}>
-                    <div className="form-group">
-                        <label htmlFor="name">Nombre de Usuario:</label>
-                        <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
-                    </div>
-
-                    <div className="form-group">
-                        <label htmlFor="password">Contraseña:</label>
-                        <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} required />
-                    </div>
-
-                    <div className="form-group">
-                        <label htmlFor="email">Correo Electrónico:</label>
-                        <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
-                    </div>
-
-                    <div className="form-group">
-                        <input type="checkbox" id="terms" name="terms" checked={formData.terms} onChange={handleChange} required />
-                        <label htmlFor="terms">Acepto los Términos y Condiciones</label>
-                    </div>
-
-                    <div className="form-group">
-                        <input type="checkbox" id="data-treatment" name="dataTreatment" checked={formData.dataTreatment} onChange={handleChange} required />
-                        <label htmlFor="data-treatment">Acepto el Tratamiento de Datos</label>
-                    </div>
-
-                    <div className="form-group">
-                        <h4>Regístrate con:</h4>
+        <div>
+            <Nav/>
+            <div className="container">
+                <div className="image-container">
+                    <img src="https://res.cloudinary.com/dtmapxnao/image/upload/v1709768800/Open-Day/LogoBiblioNube_dbrkmt.png" alt="Placeholder" />
+                </div>
+                <div className="form-container card">
+                    <h2>Registro de Usuario</h2>
+                    <form onSubmit={handleSubmit}>
                         <div className="form-group">
-                            <input type="checkbox" id="google" name="google" checked={formData.google} onChange={handleChange} />
-                            <label htmlFor="google">Google</label>
+                            <label htmlFor="name">Nombre de Usuario:</label>
+                            <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
                         </div>
+
                         <div className="form-group">
-                            <input type="checkbox" id="facebook" name="facebook" checked={formData.facebook} onChange={handleChange} />
-                            <label htmlFor="facebook">Facebook</label>
+                            <label htmlFor="password">Contraseña:</label>
+                            <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} required />
                         </div>
-                    </div>
 
-                    <button type="submit" className="register-btn">Registrarme!</button>
+                        <div className="form-group">
+                            <label htmlFor="email">Correo Electrónico:</label>
+                            <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
+                        </div>
 
-                </form>
+                        <div className="form-group">
+                            <input type="checkbox" id="terms" name="terms" checked={formData.terms} onChange={handleChange} required />
+                            <label htmlFor="terms">Acepto los Términos y Condiciones</label>
+                        </div>
+
+                        <div className="form-group">
+                            <input type="checkbox" id="data-treatment" name="dataTreatment" checked={formData.dataTreatment} onChange={handleChange} required />
+                            <label htmlFor="data-treatment">Acepto el Tratamiento de Datos</label>
+                        </div>
+
+                        <div className="form-group">
+                            <h4>Regístrate con:</h4>
+                            <div className="form-group">
+                                <input type="checkbox" id="google" name="google" checked={formData.google} onChange={handleChange} />
+                                <label htmlFor="google">Google</label>
+                            </div>
+                            <div className="form-group">
+                                <input type="checkbox" id="facebook" name="facebook" checked={formData.facebook} onChange={handleChange} />
+                                <label htmlFor="facebook">Facebook</label>
+                            </div>
+                        </div>
+
+                        <button type="submit" className="register-btn">Registrarme!</button>
+
+                    </form>
+                </div>
             </div>
         </div>
     )
